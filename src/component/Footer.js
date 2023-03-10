38a227b836d133d4b79170bbdfa9fd67ff800040
elainebautista0919 <elaine.bautista@gmail.com>
2023-03-10 00:37:59 -0500

    created footer section

---
+++ src/component/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li className='footer-li'>
+                    <a
+                        className='footer-link'
+                        href='https://github.com/elainebautista0919'
+                        target='_blank'>github</a>
+                </li>
+                <li className='footer-li'>
+                    <a
+                        className='footer-link'
+                        href='https://www.linkedin.com/in/elaine-bautista-b7916452'
+                        target='_blank'>linkedin</a>
+                </li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
