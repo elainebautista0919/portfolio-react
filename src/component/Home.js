1fabf452ce5aaa28588dbdd7dbd6d8f3051d6766
elainebautista0919 <elaine.bautista@gmail.com>
2023-03-10 00:34:25 -0500

    created home page

---
+++ src/component/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+        <section
+            id='home'
+            className='home-photo'
+            alt='desktop photo'>
+                <div className='home-textbox'>
+                    <h1>
+                        hello, i'm elaine.
+                        <br />
+                        welcome to my website.
+                    </h1>                    
+                </div>
+        </section>
+    );
+}
+
+export default Home;
---
+++ src/component/Resume.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section className='resume-section'>
+            <div className='resume-container'>
+                <h1>resume</h1>
+                    <p>download my <span></span>
+                        <a href='https://drive.google.com/file/d/1bh4QFYT7JJhAbOPKkjfBZf5Uq0Qco3NM/view?usp=share_link'
+                            target='_blank'>resume</a>
+                    </p>
+                    <br />
+
+                <h2>front end</h2>
+                <ul>
+                    <li>html</li>
+                    <li>css</li>
+                    <li>javascript</li>
+                    <li>react</li>
+                </ul>
+                     <br />
+
+                <h2>back end</h2>
+                <ul>
+                    <li>api</li>
+                     <li>express</li>
+                    <li>node</li>
+                    <li>mysql</li>
+            </ul>
+            </div>
+        </section>
+    );
+}
+
+export default Resume;
